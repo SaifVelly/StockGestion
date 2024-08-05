e796b6e00c9707dcbfb450fe820ae6ed914819ed
Mohammed Kssim <87041166+SaifVelly@users.noreply.github.com>
2024-08-05 20:02:16 +0100

    Update server.js changed origin CORS to env file

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: process.env.CORS_ORIGIN ,
     credentials: true,
 }))
 

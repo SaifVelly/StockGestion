bbdf9f339c22305ad25bc7b5c0135f7e412c6bf3
Mohammed Kssim <87041166+SaifVelly@users.noreply.github.com>
2024-08-06 15:26:09 +0100

    Update authMiddleware.js changed tokens

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,36 +3,40 @@ const User = require('../schema/userSchema');
 const jwt = require('jsonwebtoken');
 
 const authorization = asyncHandler(async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        console.log(token);
-        if (!token) {
-            res.status(401);
-            throw new Error("Not authorized, please login")
-        }
+  try {
+    // Prioritize token from cookies for compatibility
+    let token = req.cookies.token;
 
-        // Verify Token
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-
-        console.log(verified);
-
-        const user = await User.findById(verified.id).select("-password");
-
-        console.log(user);
-
-        if (!user) {
-            res.status(401);
-            throw new Error("User not found");
-        }
-
-        req.user = user;
-        next();
-
-    } catch (error) {
-        res.status(401);
-        throw new Error("Not authorized, please login"); 
+    // If cookie token is not found, check for token in authorization header
+    if (!token) {
+      const bearerHeader = req.headers['authorization'];
+      if (bearerHeader) {
+        token = bearerHeader.split(' ')[1];
+      }
     }
+
+    if (!token) {
+      res.status(401);
+      throw new Error("Not authorized, please login");
+    }
+
+    // Verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(verified.id).select("-password");
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+    next();
+
+  } catch (error) {
+    res.status(401);
+    throw new Error("Not authorized, please login"); 
+  }
 });
 
 module.exports = authorization;
-
